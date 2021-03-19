96edf6415afe3606f6f82caaad94e2b474926a61
André Vieira <vieirahbs@hotmail.com>
2021-03-19 18:04:53 -0300

    Acrescentada a mensagem Have a nice day

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -4,9 +4,9 @@ namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
             
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Good Morning!");
+            Console.WriteLine("Hello World!");            
             Console.WriteLine("How are you doing?");
+            Console.WriteLine("Have a good day");
             Console.ReadLine();
 
 
