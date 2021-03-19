59c9ba4223cbb0804f32ce311f99e637c6a50b66
André Vieira <vieirahbs@hotmail.com>
2021-03-19 17:07:53 -0300

    Acrescentada uma mensagem de Good Morning

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -3,8 +3,9 @@
 namespace projeto_teste {
     class Program {
         static void Main(string[] args) {
+            
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Good Morning!");
             Console.ReadLine();
 
 
